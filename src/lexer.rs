2f651be856c63e76144b6d1cb0f1023ad5974951
Nicholas Stienz <nickstienz@gmail.com>
2023-12-18 20:58:48 -0600

    Cleaned main and started the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,1 +1,13 @@
-pub struct Lexer {}
+pub struct Lexer<'a> {
+    characters: &'a [char],
+    position: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(text: &'a [char]) -> Self {
+        Self {
+            characters: text,
+            position: 0,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
+use lexer::Lexer;
 use std::fs::File;
 use std::io::Read;
 
 mod lexer;
 
 fn main() {
-    // Get words in the input file
-    let text = file_to_chars("check.txt");
-    let words: Vec<&str> = parse(String::from("test"));
-
-    // Create Vec of 10,000 numbers from digits in e (Euler's number)
-    let e_chars = file_to_chars("e_to_10k.txt");
-    let e_vec: Vec<usize> = e_chars
+    let e_vec: Vec<usize> = file_to_chars("e_to_10k.txt")
         .iter()
         .filter_map(|c| c.to_digit(10))
         .map(|d| d as usize)
         .collect();
 
-    // Check if word length match digit in e
+    let text = file_to_chars("check.txt");
+    let lexer = Lexer::new(&text);
+
+    /* TODO: Remake this logic vvvvv
     for (pos, (word, &digit)) in words.iter().zip(e_vec.iter()).enumerate() {
         let len = word.len();
         if len != digit {
@@ -29,10 +27,7 @@ fn main() {
             );
         }
     }
-}
-
-fn parse(t: String) -> Vec<&'static str> {
-    vec!["Test"]
+    */
 }
 
 fn file_to_chars(path: &str) -> Vec<char> {
